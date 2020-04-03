c2ecd9c21b66ff751b71a542474d1fe84511bc3c
Looqer <lukaszsienkiewicz94@gmail.com>
2020-04-03 19:03:25 +0200

    mark 1.01

--- Result.java
+++ Result.java
@@ -1,2 +1,5 @@
 public enum Result {
+    WIN,
+    TIE,
+    LOSE
 }
--- Sign.java
+++ Sign.java
@@ -1,2 +1,29 @@
 public enum Sign {
+    ROCK,
+    PAPER,
+    SCISSORS
+
+
+    static Sign[] getAll (){
+        Sign[] signsAll = new Sign[]{Sign.ROCK, Sign.PAPER, Sign.SCISSORS};
+        return signsAll;
+    }
+
+    Result getResultVs(Sign other){
+        Result presentResult = Result.WIN;
+        return presentResult;
+    }
+
+    static Sign getRandom(){
+        Sign randomSign = Sign.PAPER;
+        return randomSign;
+    }
+
+    static Sign getSignWinningOver(Sign other){
+        Sign signWinningOver = Sign.PAPER;
+        return signWinningOver;
+    }
+
+
+
 }
